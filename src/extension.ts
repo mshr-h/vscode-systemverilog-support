43996467e2f09ff3ec197fdb2b501bf2e24df1fa
yuling <asdf168gh@gmail.com>
2017-03-27 14:07:17 +0800

    add instantiate module command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the necessary extensibility types to use in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // This method is called when your extension is activated. Activation is
 // controlled by the activation events defined in package.json.
@@ -9,6 +11,13 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.languages.registerHoverProvider('systemverilog',
             new SystemVerilogHoverProvider()
+        )
+    );
+
+    // instantiate system verilog module
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.systemverilog.instantiateModule',
+            instantiateModuleInteract
         )
     );
 }
@@ -84,4 +93,119 @@ class SystemVerilogHoverProvider implements vscode.HoverProvider {
             }
         }
     }
+}
+
+function getDirectories (srcpath: string): string[] {
+    return fs.readdirSync(srcpath)
+        .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory());
+}
+
+function getFiles (srcpath: string): string[] {
+    return fs.readdirSync(srcpath)
+        .filter(file => fs.statSync(path.join(srcpath, file)).isFile());
+}
+
+function selectFile(currentDir?: string): Thenable<string> {
+    currentDir = currentDir || vscode.workspace.rootPath;
+
+    let dirs = getDirectories(currentDir);
+    // if is subdirectory, add '../'
+    if (currentDir !== vscode.workspace.rootPath) {
+        dirs.unshift('..')
+    }
+    // all files ends with '.sv'
+    let files = getFiles(currentDir)
+        .filter(file => file.endsWith('.v') || file.endsWith('.sv'));
+
+    // available quick pick items
+    let items = dirs.concat(files);
+
+    return vscode.window.showQuickPick(items)
+        .then(selected => {
+            if (!selected) {
+                return;
+            }
+
+            // if is a directory
+            let location = path.join(currentDir, selected);
+            if (fs.statSync(location).isDirectory()) {
+                return selectFile(location);
+            }
+
+            // return file path
+            return location;
+        });
+}
+
+function instantiatePort(ports: string[]): string {
+    let port = ''
+    // .NAME(NAME)
+    for (let i = 0; i < ports.length; i++) {
+        let element = ports[i];
+        port += `\t.${element}(${element})`;
+
+        if (i !== ports.length - 1) {
+            port += ',';
+        }
+        port += '\n';
+    }
+    return port;
+}
+
+function instantiateModule(srcpath: string) {
+    if (!srcpath || !fs.statSync(srcpath).isFile) {
+        return;
+    }
+
+    // remove comment
+    let content = fs.readFileSync(srcpath, 'utf8').replace(/\/\/.*/g, '').replace(/\/\*[\s\S]*?\*\//g, '');
+    if (content.indexOf('module') === -1) {
+        return;
+    }
+    // module 2001 style
+    let moduleIO = content.substring(content.indexOf('module'), content.indexOf(';'));
+    let moduleName = moduleIO.match(/module\s+\b([A-Za-z_][A-Za-z0-9_]*)\b/)[1];
+    let parametersName = [];
+    let portsName = [];
+    let lines = moduleIO.split('\n');
+
+    // find all parameters and ports
+    lines.forEach(line => {
+        line = line.trim();
+        let matched = line.match(/parameter\s+\b([A-Za-z_][A-Za-z0-9_]*)\b/);
+        if (matched !== null) {
+            parametersName.push(matched[1]);
+        }
+
+        if (line.search(/^\b(input|output|inout)\b/) !== -1) {
+            let variables = line.replace(/\b(input|output|inout|reg|wire|logic|integer|bit|byte|shortint|int|longint|time|shortreal|real|double|realtime)\b/g, '')
+                .replace(/(\[.+?\])?/g, '').replace(/\s+/g, '').split(',').forEach(variable => {
+                if (variable) {
+                    portsName.push(variable);
+                }
+            });
+        }
+    });
+
+    if (portsName.length === 0) {
+        return;
+    }
+    let inst = moduleName + '\n';
+    if (parametersName.length > 0) {
+        inst += '#(\n';
+        inst += instantiatePort(parametersName);
+        inst += ')\n';
+    }
+    inst += `u_${moduleName}(\n`;
+    inst += instantiatePort(portsName);
+    inst += ');\n';
+    return inst;
+}
+
+function instantiateModuleInteract() {
+    let filePath = path.dirname(vscode.window.activeTextEditor.document.fileName);
+    selectFile(filePath).then(srcpath => {
+        let inst = instantiateModule(srcpath);
+        vscode.window.activeTextEditor.insertSnippet(new vscode.SnippetString(inst));
+    });
 }
